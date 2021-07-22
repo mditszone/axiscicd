b4aac7cd0233e3d09b7b8188f551ec0ef3765b9e
mditszone <mditszone@gmail.com>
2021-07-22 10:11:56 +0530

    git push code changes : 22/7/2021

---
+++ axisbackapp/src/main/java/com/mdits/AxisbackappApplication.java
@@ -0,0 +1,13 @@
+package com.mdits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxisbackappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxisbackappApplication.class, args);
+	}
+
+}
